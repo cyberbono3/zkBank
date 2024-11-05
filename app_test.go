4c280649a9751790d5ee9d4e4e5c3c21effae32b
Andrei Ivasko <andrei@fantom.foundation>
2024-11-05 12:53:42 +0300

    uploadProof

--- app_test.go
+++ app_test.go
@@ -3,7 +3,10 @@ package zksec_gkr
 import (
 	"bytes"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
@@ -12,6 +15,7 @@ import (
 	"github.com/consensys/gnark/constraint/solver"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestProveAndVerify(t *testing.T) {
@@ -57,4 +61,52 @@ func TestProveAndVerify(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	uploadProof(proofHex)
+	assert.Equal(t, 1,2)
 }
+
+func uploadProof(proof string) {
+	url := "http://147.182.233.80:8080/"
+	
+	// Create a map with the data to be sent in JSON format
+	data := map[string]string{
+		"new_bob_balance": "1000000",
+		"proof_hex":       proof,
+	}
+
+	// Encode the map into JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+
+	fmt.Printf("jsonData: %s")
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Println("Response Status:", resp.Status)
+	fmt.Println("Response Body:", string(body))
+}
+
